0d9a4334abed45e08d6186deba80bc251ff45165
Garrett <ggchan@calpoly.edu>
2017-05-09 01:43:48 -0700

    lab2 stuff

--- lab2/process.py
+++ lab2/process.py
@@ -2,15 +2,16 @@ class Process():
     arrival_time = 0
     burst_time = 0
     time_remaining = 0
-    time_completed = -1
+    turnaround_time = -1
     waiting_time = -1
     response_time = -1
+    job_no = -1
 
     def __init__(self, arrival_time, burst_time):
         self.arrival_time = arrival_time
         self.burst_time = burst_time
         self.time_remaining = burst_time
-        self.time_completed = -1
+        self.turnaround_time = -1
         self.waiting_time = -1
         self.response_time = -1
 
--- lab2/schedSim.py
+++ lab2/schedSim.py
@@ -36,15 +36,68 @@ def getSchedule(file):
         procs.append(proc)
     return procs
 
+def sort_procs(procs):
+    procs = sorted(procs, key=lambda x: x.arrival_time, reverse=False)
+    cur_job_no = 0
+    for proc in procs:
+        proc.job_no = cur_job_no
+        cur_job_no += 1
+    return procs
+
 def executeFIFO(procs):
     time = 0
     for proc in procs:
+        proc.waiting_time = time
+        proc.response_time = time + 1
+        proc.turnaround_time = time + proc.burst_time - proc.arrival_time
+        time += proc.burst_time
+    return procs
 
-def executeRR(procs):
-    time = 0
+def executeRR(procs, quantum):
+    time = procs[0].arrival_time
+    procs_left = len(procs)
+    while (procs_left > 0):
+        for proc in procs:
+            if (proc.time_remaining <= quantum and proc.time_remaining > 0):
+                time += proc.time_remaining
+                proc.time_remaining = 0
+                procs_left -= 1
+                proc.waiting_time = time - proc.burst_time - proc.arrival_time
+                proc.response_time = proc.waiting_time + 1
+                proc.turnaround_time = time - proc.arrival_time
+            elif (proc.time_remaining > 0):
+                time += quantum
+                proc.time_remaining -= quantum
+
+    return procs
 
 def executeSRJN(procs):
     time = 0
+    procs_left = len(procs)
+    while (procs_left > 0):
+        
+    return procs
+
+def printProcStats(procs):
+    for proc in procs:
+        response = proc.response_time
+        turnaround = proc.turnaround_time
+        wait = proc.waiting_time
+        print("Job %3d -- Response %3.2f Turnaround %3.2f Wait %3.2f" % (proc.job_no, response, turnaround, wait))
+
+def printAverageStats(procs):
+    avg_response = 0.0
+    avg_turnaround = 0.0
+    avg_wait = 0.0
+    proc_count = len(procs)
+    for proc in procs:
+        avg_response += proc.response_time
+        avg_turnaround += proc.turnaround_time
+        avg_wait += proc.waiting_time
+    avg_response /= proc_count
+    avg_turnaround /= proc_count
+    avg_wait /= proc_count
+    print("Average -- Response %3.2f Turnaround %3.2f Wait %3.2f" % (avg_response, avg_turnaround, avg_wait))
 
 def main():
     if (len(sys.argv) < 2):
@@ -57,9 +110,15 @@ def main():
     print("Algorithm " + algorithm)
     print("Quantum " + str(quantum))
     print(str(len(procs)))
+    new_procs = sort_procs(procs)
     if (algorithm == "FIFO"):
-        new_procs = sorted(procs, key=lambda x: x.arrival_time, reverse=False)
-        executeFIFO(new_procs)
+        new_procs = executeFIFO(new_procs)
+    elif (algorithm == "RR"):
+        new_procs = executeRR(new_procs, quantum)
+    else:
+        new_procs = executeSJRN(new_procs)
+    printProcStats(new_procs)
+    printAverageStats(new_procs)
 
 if __name__ == "__main__":
     main()
