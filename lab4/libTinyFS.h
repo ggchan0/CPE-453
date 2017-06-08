f110bf697af5b8cf2e5cb5c01fa22300fa6320b9
Garrett <ggchan@calpoly.edu>
2017-06-08 02:39:04 -0700

    verified mount/unmount functionality

--- lab4/libTinyFS.h
+++ lab4/libTinyFS.h
@@ -2,6 +2,7 @@
 #define LIBTINYFS_H
 
 #include "tinyFS.h"
+#include "TinyFS_errno.h"
 
 typedef struct freeBlock {
    int block_num;
@@ -27,5 +28,25 @@ typedef struct openFile {
    int cur_block;
    int cur_position;
 } openFile;
+
+int tfs_mkfs(char *filename, int nBytes);
+
+int tfs_mount(char *filename);
+
+int tfs_unmount(void);
+
+fileDescriptor tfs_openFile(char *name);
+
+int tfs_closeFile(fileDescriptor FD);
+
+int tfs_writeFile(fileDescriptor FD, char *buffer, int size);
+
+int tfs_deleteFile(fileDescriptor FD);
+
+int tfs_readByte(fileDescriptor FD, char *buffer);
+
+int tfs_seek(fileDescriptor FD, int offset);
+
+int getNumFreeBlocks();
 
 #endif
